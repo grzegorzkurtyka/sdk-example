d46f641b27bfa4fa68066c912396e513f565eb85
Grzegorz Kurtyka <grzegorz.kurtyka@schibsted.coml>
2020-04-17 19:27:53 +0200

    Add example of using multifactor auth

--- browser/index.js
+++ browser/index.js
@@ -2,7 +2,7 @@
 
 import 'regenerator-runtime/runtime';
 
-import { Identity, Payment, Monetization } from '@schibsted/account-sdk-browser';
+import { Identity, Payment, Monetization } from 'account-sdk-browser';
 window.Identity = Identity; // To be able to play around with it in the console...
 window.Payment = Payment;
 window.Monetization = Monetization;
